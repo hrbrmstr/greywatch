e9e5b65850beeb1feeb4cd26191d0612a099b63c
hrbrmstr <bob@rud.is>
2021-03-28 05:45:13 -0400

    Initial Commit

---
+++ greywatch/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  greywatch
+//
+//  Created by boB Rudis on 3/28/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ greywatch/greywatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  greywatchApp.swift
+//  greywatch
+//
+//  Created by boB Rudis on 3/28/21.
+//
+
+import SwiftUI
+
+@main
+struct greywatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
